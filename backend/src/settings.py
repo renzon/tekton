8ad36e51f9141799d821092937eb4077ad60e0e1
renzon <renzon@gmail.com>
2014-06-30 23:16:33 -0300

    Warmup loading modules and page error configuration

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -4,12 +4,15 @@ from tekton.gae.middleware.json_middleware import JsonResponseMiddleware
 from config.tmpl_middleware import TemplateMiddleware, TemplateWriteMiddleware
 from tekton.gae.middleware.email_errors import EmailMiddleware
 from tekton.gae.middleware.parameter import RequestParamsMiddleware
+from tekton.gae.middleware.redirect import RedirectMiddleware
 from tekton.gae.middleware.router_middleware import RouterMiddleware, ExecutionMiddleware
 from tekton.gae.middleware.webapp2_dependencies import Webapp2Dependencies
 
 SENDER_EMAIL = 'renzon@gmail.com'
 DEFAULT_LOCALE = 'pt_BR'
 DEFAULT_TIMEZONE = 'America/Sao_Paulo'
+TEMPLATE_404_ERROR = 'base/404.html'
+TEMPLATE_400_ERROR = 'base/400.html'
 MIDDLEWARE_LIST = [TemplateMiddleware,
                    EmailMiddleware,
                    Webapp2Dependencies,
@@ -17,6 +20,7 @@ MIDDLEWARE_LIST = [TemplateMiddleware,
                    RouterMiddleware,
                    ExecutionMiddleware,
                    TemplateWriteMiddleware,
-                   JsonResponseMiddleware]
+                   JsonResponseMiddleware,
+                   RedirectMiddleware]
 
 
--- backend/src/web/warmup.py
+++ backend/src/web/warmup.py
@@ -1,16 +1,28 @@
+import logging
 import sys
+import time
 import os
 
-#Put lib on path, once Google App Engine does not allow doing it directly
-sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+# Put lib on path, once Google App Engine does not allow doing it directly
+src_path = os.path.dirname(__file__)
+src_path = os.path.join(src_path, '..')
+src_path = os.path.normpath(src_path)
+sys.path.append(os.path.join(src_path, 'lib'))
+from gaepermission import facade
 
 import webapp2
 
 
 class BaseHandler(webapp2.RequestHandler):
     def get(self):
-        pass
+        # This import all paths so its good pre initialize them here
+        init_time = time.time()
+        facade.web_path_security_info()
+        end_time = time.time()
+        delta_seconds = (end_time - init_time)
+        logging.info('Startup: %s miliseconds' % (delta_seconds*1000))
 
 
-app = webapp2.WSGIApplication([("/.*", BaseHandler)], debug=False)
+
+app = webapp2.WSGIApplication([('/.*', BaseHandler)], debug=False)
 
