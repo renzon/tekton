aa462e01b08a3e8622d5e21cad034140e61463c7
renzon <renzon@gmail.com>
2014-07-03 00:21:48 -0300

    fixing permission

--- backend/plugins/appengine/web/permission/home.py
+++ backend/plugins/appengine/web/permission/home.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from apps.permission_app.model import ADMIN
+from permission_app.model import ADMIN
 from gaecookie.decorator import no_csrf
 from gaepermission import facade
 from config.tmpl_middleware import TemplateResponse
