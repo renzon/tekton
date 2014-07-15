34eeb4593d5ebe48772a1f810c8052925d296188
renzon <renzon@gmail.com>
2014-07-15 11:33:39 -0300

    #14 upgrading virtuaenvl files

--- backend/venv/venv.bat
+++ backend/venv/venv.bat
@@ -12,20 +12,20 @@ call :to_console "Checking up dependencies"
 pip install -r dev_requirements.txt --upgrade || goto :error 
 
 
-cd ..\plugins\appengine || goto :error
+cd ..\appengine || goto :error
 
 IF NOT EXIST %cd%\lib (
 call :to_console "Creating symlink on plugins\appengine so installed libs become visible to Google App Engine"
-MKLINK /D lib ..\..\venv\Lib\site-packages
+MKLINK /D lib ..\venv\Lib\site-packages
 )
 
 IF NOT EXIST %cd%\apps (
 call :to_console "Creating symlink on plugins\appengine so apps become visible to Google App Engine"
-MKLINK /D apps ..\..\apps
+MKLINK /D apps ..\apps
 )
 
 
-cd ..\..\venv || goto :error
+cd ..\venv || goto :error
 call :to_console "virtualenv and dependencies installed"
 goto:EOF
 
--- backend/venv/venv.sh
+++ backend/venv/venv.sh
@@ -10,19 +10,18 @@ cd $(dirname $0) && cd ..
 
 to_console "creating virtual env on venv folder"
 virtualenv -p /usr/bin/python2.7 venv
-cd venv
 
 to_console "Activating virtualenv"
-source bin/activate
+source venv/bin/activate
 
 to_console "Checking up dependencies"
-pip install -r dev_requirements.txt --upgrade
+pip install -r venv/dev_requirements.txt
 
-cd ../plugins/appengine
+cd appengine
 
 if [ ! -d lib ]; then
     to_console "Creating symlink on plugins/appengine/lib so installed libs become visible to Google App Engine"
-    ln -s ../../venv/lib/python2.7/site-packages lib
+    ln -s ../venv/lib/python2.7/site-packages lib
 fi
 
 if [ ! -d apps ]; then
