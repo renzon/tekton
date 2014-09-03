c836f7ad05b27d5323108a41131a0dfc2036c4cb
renzon <renzon@gmail.com>
2014-09-03 00:39:03 -0300

    close #15

--- backend/appengine/routes/logout.py
+++ backend/appengine/routes/logout.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from google.appengine.api.users import create_logout_url, get_current_user
 from gaepermission import facade
 from gaepermission.decorator import login_required
 from tekton.gae.middleware.redirect import RedirectResponse
@@ -8,4 +9,8 @@ from tekton.gae.middleware.redirect import RedirectResponse
 @login_required
 def index(_resp):
     facade.logout(_resp).execute()
-    return RedirectResponse('/')
+    redirect_url = '/'
+    google_user = get_current_user()
+    if google_user:
+        redirect_url = create_logout_url(redirect_url)
+    return RedirectResponse(redirect_url)
