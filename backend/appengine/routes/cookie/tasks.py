84156a3ff720f2aae862e9dd69696507c925182b
renzon <renzon@gmail.com>
2014-11-14 14:53:32 -0200

    Setting up task security

--- backend/appengine/routes/cookie/tasks.py
+++ backend/appengine/routes/cookie/tasks.py
@@ -2,8 +2,10 @@
 from __future__ import absolute_import, unicode_literals
 from gaecookie import facade
 from gaecookie.decorator import no_csrf
+from gaepermission.decorator import login_not_required
 
 
+@login_not_required
 @no_csrf
 def renew():
     facade.renew().execute()
