99c66cb9fddb6c0af77ed43df3b73e3bc4d0feba
renzon <renzon@gmail.com>
2014-11-20 13:54:18 -0200

    close #28

--- backend/test/base.py
+++ backend/test/base.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-
 import unittest
 from google.appengine.ext import testbed
+import webapp2
+from webapp2_extras import i18n
+import settings
 
 
+#workaround for i18n. without this test will not run
+
+app = webapp2.WSGIApplication(
+    [webapp2.Route('/', None, name='upload_handler')])
+
+request = webapp2.Request({'SERVER_NAME': 'test', 'SERVER_PORT': 80,
+                           'wsgi.url_scheme': 'http'})
+request.app = app
+app.set_globals(app=app, request=request)
+
+i18n.default_config['default_locale'] = 'en_US'
+i18n.default_config['default_timezone'] = settings.DEFAULT_TIMEZONE
+
+# End of workaround
 
 class GAETestCase(unittest.TestCase):
     def setUp(self):
