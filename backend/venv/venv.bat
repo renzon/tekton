0d55e1c29115e88026b864158b28fd8db1babc61
renzon <renzon@gmail.com>
2015-01-05 14:07:40 -0200

    #23 windows bat fixed

--- backend/venv/venv.bat
+++ backend/venv/venv.bat
@@ -1,5 +1,7 @@
 call :to_console "Setting up virtualenv on venv"
 
+cd %~dp0 || goto :error
+
 call :to_console "creating virtual env on venv folder"
 virtualenv . || goto :error 
 
@@ -7,22 +9,28 @@ call :to_console "Activating virtualenv"
 call Scripts\activate || goto :error 
 
 
+IF "%1"=="" (
 call :to_console "Checking up dependencies"
-
-pip install -r dev_requirements.txt --upgrade || goto :error 
-
-
+pip install -r dev_requirements.txt --upgrade || goto :error
+) ELSE (
+call :to_console "Checking up dependencies with proxy %1"
+pip install -r dev_requirements.txt --upgrade --proxy=%1 || goto :error
+)
 cd ..\appengine || goto :error
 
-IF NOT EXIST %cd%\lib (
-call :to_console "Creating symlink on plugins\appengine so installed libs become visible to Google App Engine"
-MKLINK /D lib ..\venv\Lib\site-packages
+IF EXIST %cd%\lib (
+rmdir %cd%\lib
 )
 
-IF NOT EXIST %cd%\apps (
+call :to_console "Creating symlink on plugins\appengine so installed libs become visible to Google App Engine"
+MKLINK /D lib ..\venv\Lib\site-packages
+
+IF EXIST %cd%\apps (
+rmdir %cd%\apps
+)
+
 call :to_console "Creating symlink on plugins\appengine so apps become visible to Google App Engine"
 MKLINK /D apps ..\apps
-)
 
 
 cd ..\venv || goto :error
