16a0acab081849ad891fdd69fcfde4d05b48618c
renzon <renzon@gmail.com>
2015-01-29 02:22:14 -0200

    Removing updown, once it is documented on App Engine Python Book

--- backend/appengine/routes/updown/download.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
-from gaecookie.decorator import no_csrf
-
-
-@no_csrf
-def index(_handler, blob_key):
-    _handler.send_blob(blob_key)
