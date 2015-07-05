9eeab5a29370ccbdfaf115a8c5bdf2054c3aba40
renzon <renzon@gmail.com>
2015-07-05 11:03:04 -0300

    # 50 testing passworldess check email

--- backend/test/login_tests/passwordless.py
+++ backend/test/login_tests/passwordless.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from base import GAETestCase
-from mock import patch
+from mock import patch, Mock
 from routes.login import passwordless
 from routes.login.passwordless import check
 import settings
+from tekton.gae.middleware.redirect import RedirectResponse
 from tekton.router import to_path
 
 
@@ -21,3 +22,14 @@ class SendEmailTests(GAETestCase):
         passwordless.send_email(email)
         send_link_mock.assert_called_once_with(email,
                                                settings.APP_URL + to_path(check, ret_path='/'))
+class CheckEmailTests(GAETestCase):
+    @patch('routes.login.passwordless.facade.login_passwordless')
+    def test_success(self, login_mock):
+        http_resp=Mock()
+        ticket = 'ticket'
+        cmd_mock=Mock()
+        login_mock.return_value=cmd_mock
+        response=passwordless.check(http_resp, ticket)
+        self.assertIsInstance(response,RedirectResponse)
+        login_mock.assert_called_once_with(ticket, http_resp)
+        cmd_mock.execute.assert_called_once_with()
