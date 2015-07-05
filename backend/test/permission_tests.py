15de730883b33e7fbf76dc52e55337dde3f7fd35
renzon <renzon@gmail.com>
2015-07-05 11:24:58 -0300

    # 50 testing permission list_usrs

--- backend/test/permission_tests.py
+++ backend/test/permission_tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from base import GAETestCase
+from gaepermission import facade
 from routes.permission import home, admin
 
 
@@ -14,3 +15,12 @@ class AdminTests(GAETestCase):
     def test_render(self):
         response = admin.index()
         self.assert_can_render(response)
+
+    def test_list_users_empty(self):
+        response = admin.list_users()
+        self.assert_can_serialize_as_json(response)
+
+    def test_list_users(self):
+        facade.save_user_cmd('foo@gmail.com')
+        response = admin.list_users()
+        self.assert_can_serialize_as_json(response)
