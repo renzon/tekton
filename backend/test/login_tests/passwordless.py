5370d5ce2b3c19ef7e15d5c86f19e0fc6fac18e7
renzon <renzon@gmail.com>
2015-07-05 10:53:41 -0300

    # 50 testing passworldess send email

--- backend/test/login_tests/passwordless.py
+++ backend/test/login_tests/passwordless.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from base import GAETestCase
+from mock import patch
 from routes.login import passwordless
+from routes.login.passwordless import check
+import settings
+from tekton.router import to_path
 
 
 class IndexTests(GAETestCase):
     def test_success(self):
         response = passwordless.index()
         self.assert_can_render(response)
+
+
+class SendEmailTests(GAETestCase):
+    @patch('routes.login.passwordless.facade.send_passwordless_login_link')
+    def test_success(self, send_link_mock):
+        email = 'foo@bar.com'
+        passwordless.send_email(email)
+        send_link_mock.assert_called_once_with(email,
+                                               settings.APP_URL + to_path(check, ret_path='/'))
