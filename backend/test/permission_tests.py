a9741ab4cf1bcb5cd7de3da292682e65c80210f2
renzon <renzon@gmail.com>
2015-07-05 11:35:22 -0300

    # 50 testing permission update

--- backend/test/permission_tests.py
+++ backend/test/permission_tests.py
@@ -21,6 +21,26 @@ class AdminTests(GAETestCase):
         self.assert_can_serialize_as_json(response)
 
     def test_list_users(self):
-        facade.save_user_cmd('foo@gmail.com')
+        email = 'foo@gmail.com'
+        facade.save_user_cmd(email).execute()
         response = admin.list_users()
         self.assert_can_serialize_as_json(response)
+        self.assertEqual(email, response.context['users'][0]['email'])
+
+    def test_update_user(self):
+        email = 'foo@gmail.com'
+        facade.save_user_cmd(email).execute()
+        user = facade.get_user_by_email(email)()
+        self.assertListEqual([''], user.groups)
+
+        ADMIN = 'ADMIN'
+        admin.update(str(user.key.id()), [ADMIN])
+
+        user = user.key.get()
+        self.assertListEqual([ADMIN], user.groups)
+
+        MANAGER = 'MANAGER'
+        admin.update(str(user.key.id()), [ADMIN, MANAGER])
+
+        user = user.key.get()
+        self.assertListEqual([ADMIN, MANAGER], user.groups)
